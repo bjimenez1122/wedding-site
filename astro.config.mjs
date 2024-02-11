b63ea7af1c467d2273ac77dda92a9426c9ce5d20
Bryan Jimenez-Rojas <bryan.jimenez327@gmail.com>
2024-02-11 11:46:47 -0500

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://wedding.jimenezdumeng.com',
+	// base: 'wedding-site',
+	integrations: [sitemap()],
+});
